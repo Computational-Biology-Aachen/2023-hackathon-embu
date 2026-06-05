cb3d6f24b7e220c092d0666229f2a38dabce9ba7
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-06-05 10:23:04 +0200

    fix: unify config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,7 @@
 import { dev } from "$app/environment";
 
-export const title = "Hackathon Watamu";
-export const description = "Hackathon Watamu";
+export const title = "Hackathon Embu";
+export const description = "Hackathon Embu";
 export const url = dev
   ? "http://localhost:5173/"
-  : "https://computational-biology-aachen.github.io/2022-hackathon-watamu";
+  : "https://computational-biology-aachen.github.io/2023-hackathon-embu/";
